a04334c6c4d93951c8db6b928b8f2b1ce8fdb80a
Baofeng Guo <peterguo1983@gmail.com>
2022-02-04 16:58:58 -0800

    Updated ProductTag.js.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,31 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+     // define id column
+     id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // define product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // defines tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag model's id 
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
