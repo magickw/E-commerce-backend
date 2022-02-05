ffe06fe96668119d9c8b7cd76d11d17f7a6c2cee
Baofeng Guo <peterguo1983@gmail.com>
2022-02-04 17:03:15 -0800

    Updated index.js.

--- models/index.js
+++ models/index.js
@@ -5,8 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id',
+});
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
