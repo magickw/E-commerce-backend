b1eac14ac1cf5fdba815c3bd772678a63141b3f3
Baofeng Guo <peterguo1983@gmail.com>
2022-02-04 16:48:37 -0800

    Updated product.js.

--- models/Category.js
+++ models/Category.js
@@ -6,13 +6,14 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // define id column
     id: {
       type: DataTypes.INTEGER, 
       allowNull : false, 
       primaryKey: true,
       autoIncrement: true
     },
+    // define category_name column
     category_name: {
     type: DataTypes.STRING,
     allowNull: false
